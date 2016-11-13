bb18b723b016c5d977e35da9e9d5813d6add350e
Emin Mastizada <emin@mastizada.com>
2016-11-13 21:32:11 +0300

    i18n url patterns

--- common/apps.py
+++ common/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     name = 'common'
     verbose_name = "Common Application"
-
-    # def ready(self):
-    #     import common.signals
