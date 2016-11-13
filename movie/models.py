56c93a3550d7768b4f5fb86fa2a767c9ba606a88
Emre Eroğlu <emreeroglu@itu.edu.tr>
2016-11-13 19:56:14 +0300

    imdb search functions added

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+
+    imdb_id = models.CharField(
+        blank=False, null=False, max_length=10
+    )
+
+    title = models.CharField(
+        _("Movie Title"), blank=False, null=False, max_length=100
+    )
+
+    language = models.CharField(
+        _("Movie Language"), blank=True, null=True, max_length=20
+    )
+
+    release_date = models.DateField(
+        _("Release Date"), blank=True, null=True
+    )
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
